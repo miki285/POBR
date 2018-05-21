bd9914579bbe1c299d8b439f9308341c4cc2b280
Mikołaj Krzyszczak <miwo.krzyszczak@gmail.com>
2018-05-21 08:29:46 +0200

    dsd

--- math_minmax.h
+++
@@ -1,8 +0,0 @@
-#ifndef math_minmax
-#define math_minmax
-#pragma once
-
-unsigned int minimum(unsigned int R, unsigned int G, unsigned int B);
-unsigned int maximum(unsigned int R, unsigned int G, unsigned int B);
-
-#endif
